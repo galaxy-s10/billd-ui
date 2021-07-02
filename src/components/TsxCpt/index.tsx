d6ba5d0d64215fae19208e9340d680464c5fbdc2
galaxy-s10 <2274751790@qq.com>
2021-07-02 10:41:59 +0800

    0702-1

--- src/components/TsxCpt/index.tsx
+++ src/components/TsxCpt/index.tsx
@@ -1,11 +1,22 @@
 // const { hello } = require("../../utils/hello"); //node模块化
 import { hello } from "../../utils/hello"; //es6模块化
+import Input from "../../../components/input/index";
+
+console.log(Input,2342);
 
 // hello(123);  //报错:类型“number”的参数不能赋给类型“string”的参数。ts(2345)
 hello("typescript");
 
 export default {
+  components: {
+    HInput:Input,
+  },
   render() {
-    return <div>我是tsxCpt</div>;
+    return (
+      <div>
+        我是tsxCpt
+        <h-input></h-input>
+      </div>
+    );
   },
 };
