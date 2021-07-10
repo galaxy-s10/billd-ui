5b7299a44058e565a38512c91ef954495ab07471
galaxy-s10 <2274751790@qq.com>
2021-07-10 18:09:15 +0800

    0710

--- src/Appx.jsx
+++ src/Appx.jsx
@@ -5,16 +5,21 @@ export default {
     HSwitch: Switch
   },
   render() {
+    console.log(
+      <div>
+        <h-switch dgdf="sdgs" sg="fsdf" switchVal="false" vOn:clickSwitch={(x, e) => this.clickSwitch(x, e)}></h-switch>
+      </div>
+    );
     return (
       <div>
-        <h-switch vOn:clickSwitch={(x,e)=>this.clickSwitch(x,e)}></h-switch>
+        <h-switch vOn:clickSwitch={(x, e) => this.clickSwitch(x, e)}></h-switch>
       </div>
     );
   },
   methods: {
-    clickSwitch(v,e) {
+    clickSwitch(v, e) {
       console.log(arguments);
-      console.log(v,e);
+      console.log(v, e);
     }
   }
 };
